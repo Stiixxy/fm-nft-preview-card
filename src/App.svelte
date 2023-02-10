f49f3c8f275e01870b2fbd02e14fc1b3fab0d184
Stiixxy <veenman.stijn@gmail.com>
2023-02-10 22:23:06 +0100

    added base card layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,33 @@
 <script lang="ts">
 </script>
 
-<main>
-  <h1 class="bg-darkblue-100">Hello</h1>
-</main>
+<div class="grid h-screen place-items-center">
+  <main class="grid max-w-xs gap-4 rounded-xl bg-darkblue-200 p-6">
+    <img src="/image-equilibrium.jpg" alt="equilibrium" class="rounded-lg" />
+
+    <h1 class="text-xl font-semibold text-white">Equilibrium #3429</h1>
+
+    <p class="text-softblue">
+      Our Equilibrium collection promotes balance and calm
+    </p>
+    <div class="flex justify-between">
+      <p class="flex items-center tracking-tight text-cyan">
+        <svg width="11" height="18" xmlns="http://www.w3.org/2000/svg"
+          ><path
+            d="M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z"
+            fill="#00FFF8"
+          /></svg
+        >
+        <span class="ml-1">0.041</span><span class="ml-0.5">ETH</span>
+      </p>
+      <p class="flex items-center gap-2 text-softblue">
+        <svg width="17" height="17" xmlns="http://www.w3.org/2000/svg"
+          ><path
+            d="M8.305 2.007a6.667 6.667 0 1 0 0 13.334 6.667 6.667 0 0 0 0-13.334Zm2.667 7.334H8.305a.667.667 0 0 1-.667-.667V6.007a.667.667 0 0 1 1.334 0v2h2a.667.667 0 0 1 0 1.334Z"
+            fill="#8BACD9"
+          /></svg
+        >3 days left
+      </p>
+    </div>
+  </main>
+</div>
